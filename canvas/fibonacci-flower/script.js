e59e119773432b12b408c8a222255f3fa62cd8e6
Dayton Outar <dayton.outar@gmail.com>
2021-06-21 18:48:32 -0500

    Rainbow burst

--- canvas/fibonacci-flower/script.js
+++ canvas/fibonacci-flower/script.js
@@ -3,35 +3,37 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+//ctx.globalCompositeOperation = 'destination-over';
 
 let number = 0;
 let scale = 10;
+let hue = 0;
 
 function drawFlower() {
-    let angle = number * 1;
+    let angle = number * 3.9; // Each coefficient gives a different shape
     let radius = scale * Math.sqrt(number);
     let positionX = radius * Math.sin(angle) + (canvas.width / 2);
     let positionY = radius * Math.cos(angle) + (canvas.height / 2);
 
-    ctx.fillStyle = '#f35500';
-    ctx.strokeStyle = '#03abf0';
-    ctx.lineWidth = 5;
+    ctx.fillStyle = `hsl(${ hue }, 100%, 50%)`;
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 4;
     ctx.beginPath();
-    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
+    ctx.arc(positionX, positionY, 8, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
 
-    number+=10;
+    number++;
+    hue++;
 }
 
 function animate() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    //if (number < 500) {
-        drawFlower();
-    //}
-    
+    drawFlower();
+
+    if (number > 1000) return;
 
     requestAnimationFrame(animate);
 }
