0c64cd4476148d44208dd5227a14704dfb235905
Dayton Outar <dayton.outar@gmail.com>
2021-07-17 16:44:30 -0500

    particles without the connectors

--- canvas/particles-effect/script.js
+++ canvas/particles-effect/script.js
@@ -10,7 +10,6 @@ let mouse = {
     radius: (canvas.height / 80) * (canvas.width / 80)
 }
 
-
 window.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
@@ -29,7 +28,8 @@ class Particle {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle= '#8c5523';
+        ctx.fillStyle= this.color;
+        ctx.fill();
         ctx.closePath();
     }
 
@@ -40,5 +40,63 @@ class Particle {
         if (this.y > canvas.height || this.y < 0) {
             this.directionY = -this.directionY;
         }
+
+        // Check for collision between mouse and particles
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy*dy);
+
+        if (distance < mouse.radius + this.size) {
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10; 
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+            }
+        }
+
+        // Move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+
+        // Draw particle
+        this.draw();
     }
 }
+
+let particles;
+
+// Create particle array
+function init() {
+    particles = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#8c5523';
+
+        particles.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+// Animate
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight); // implied as window.innerWidth
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+    }
+}
+
+init();
+animate();
