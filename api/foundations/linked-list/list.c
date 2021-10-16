15872d3962fd1906495458a61b0757708242b812
Dayton Outar <dayton.outar@gmail.com>
2021-10-16 13:14:59 -0500

    Final on linked list in C

--- api/foundations/linked-list/list.c
+++ api/foundations/linked-list/list.c
@@ -28,10 +28,25 @@ node_t *create_new_node(int value) {
 }
 
 node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
-    node_to_insert->next = head;
+    node_to_insert->next = *head;
     *head = node_to_insert;
-    
+
     return node_to_insert;
+}
+
+node_t *find_node(node_t *head, int value) {
+    node_t *tmp = head;
+    while (tmp != NULL) {
+        if (tmp->value == value) return tmp;
+        tmp = tmp->next;
+    }
+
+    return NULL;
+}
+
+node_t *insert_after_node(node_t *node_to_insert_after, node_t *newnode) {
+    newnode->next = node_to_insert_after->next;
+    node_to_insert_after->next = newnode;
 }
 
 int main() {
@@ -40,11 +55,18 @@ int main() {
 
     for (int i = 1; i < 7; i++) {
         tmp = create_new_node((8 * i));
-        insert_at_head(head, tmp);
+        insert_at_head(&head, tmp);
     }    
 
     printlist(head);
     // Outputs: 48 --> 40 --> 32 --> 24 ...
+
+    insert_after_node(tmp, create_new_node(75));
+
+    printlist(head);
+
+    tmp = find_node(head, 24);
+    printf("Found node with value %d\n", tmp->value);
 
     return 0;
 }
