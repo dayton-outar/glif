7474f759902920bacb579af9e84a6655063220e5
Dayton Outar <dayton.outar@gmail.com>
2021-10-16 19:08:04 -0500

    Doubly linked list in C

--- api/foundations/doubly-linked-list-c/list.c
+++ api/foundations/doubly-linked-list-c/list.c
@@ -1,11 +1,12 @@
 // To compile
-// cc list.c -o list
+// gcc -g -o list list.c
 #include <stdlib.h>
 #include <stdio.h>
 
 struct node {
     int value;
     struct node* next;
+    struct node* prev;
 };
 typedef struct node node_t;
 
@@ -23,14 +24,17 @@ node_t *create_new_node(int value) {
     node_t *result = malloc(sizeof(node_t)); // Allocate on heap
     result->value = value;
     result->next = NULL;
-
+    result->prev = NULL;
     return result;
 }
 
 node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
     node_to_insert->next = *head;
+    if (*head != NULL) {
+        (*head)->prev = node_to_insert;
+    }
     *head = node_to_insert;
-
+    node_to_insert->prev = NULL;
     return node_to_insert;
 }
 
@@ -46,24 +50,27 @@ node_t *find_node(node_t *head, int value) {
 
 node_t *insert_after_node(node_t *node_to_insert_after, node_t *newnode) {
     newnode->next = node_to_insert_after->next;
+    if (newnode->next != NULL) {
+        newnode->next->prev = node_to_insert_after;
+    }
+    newnode->prev = node_to_insert_after;
     node_to_insert_after->next = newnode;
 }
 
 void remove_node(node_t **head, node_t *node_to_remove) {
     if (*head == node_to_remove) {
         *head = node_to_remove->next;
-        return;
-    } else {
-        // find the previous node in the list
-        node_t* tmp = *head;
-        while (tmp != NULL && tmp->next != node_to_remove) {
-            tmp = tmp->next;
+        if (*head != NULL) {
+            (*head)->prev = NULL;
         }
-        if (tmp == NULL) return;
-
-        tmp->next = node_to_remove->next;
-        node_to_remove->next = NULL;
+    } else {
+        node_to_remove->prev->next = node_to_remove->next;
+        if (node_to_remove->next != NULL) {
+            node_to_remove->next->prev = node_to_remove->prev;
+        }
     }
+    node_to_remove->next = NULL;
+    node_to_remove->prev = NULL;
     return;
 }
 
@@ -85,6 +92,10 @@ int main() {
 
     tmp = find_node(head, 24);
     printf("Found node with value %d\n", tmp->value);
+
+    remove_node(&head, tmp);
+
+    printlist(head);
 
     return 0;
 }
