092aee19bdf8a5d61c1a3b1fcc82ff385e85650d
Dayton Outar <dayton.outar@gmail.com>
2021-10-30 16:56:36 -0500

    More interpretations of binary trees

---
+++ api/foundations/data-structures/trees/binary-tree/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "binary-tree.h"
+
+int main() {
+    Stack<int> s;
+
+    s.push(12);
+    s.push(14);
+    s.push(80);
+
+    int t;
+    while (!s.isEmpty()) {
+        t = s.pop();
+        std::cout << "Popped: " << t << std::endl;
+    }
+
+    std::cout << "Now let's try this with a stack using linked list." << std::endl;
+
+    LLStack<float> f;
+
+    f.push(34.50);
+    f.push(12.33);
+    f.push(25.50);
+    f.push(77.05);
+
+    float b;
+    while (!f.isEmpty()) {
+        b = f.pop();
+        std::cout << "Popped: " << b << std::endl;
+    }
+
+    std::cout << "Done!" << std::endl;
+}
