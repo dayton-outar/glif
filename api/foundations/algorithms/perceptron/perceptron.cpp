0b23ce4797b2f521db360707a37c9bf4441e0af4
dayton-outar <dayton.outar@gmail.com>
2021-11-21 14:23:05 -0500

    Cross Entropy Loss

--- api/foundations/algorithms/perceptron/perceptron.cpp
+++ api/foundations/algorithms/perceptron/perceptron.cpp
@@ -1,6 +1,8 @@
 // Credit: Perceptron Algorithm with Code Example: https://youtu.be/-KLnurhX-Pg
+// g++ perceptron.cpp -o perceptron
 #include <vector>
 #include <utility>
+#include <cmath>
 
 #include <iostream>
 
@@ -24,6 +26,21 @@ int perceptron(vector<pair<float, float>> &iw, float threshold) {
     return step(wSum, threshold);
 }
 
+float cross_entropy(vector<pair<float, int>> &id) {
+    float loss = 0;
+    int n = id.size();
+
+    for (vector<pair<float,int>>::const_iterator it = id.begin(); it != id.end(); it++) {
+        float wSum = it->first; // add weights
+        float y = static_cast<float>(it->second);
+        float cl = -( (y * log10(wSum)) + ((1 - y) * log10(1 - wSum)) );
+        loss += cl;
+        cout << cl << endl;
+    }
+
+    return loss / n;
+}
+
 int main() {
     vector<pair<float, float>> iw;
     //
@@ -35,5 +52,15 @@ int main() {
 
     int output = perceptron(iw, threshold);
 
-    cout << output << endl;
+    cout << "Output: " << output << endl;
+
+    vector<pair<float, int>> id;
+    id.push_back(make_pair( 0.26, 1));
+    id.push_back(make_pair( 0.20, 0));
+    id.push_back(make_pair( 0.48, 1));
+    id.push_back(make_pair( 0.30, 0));
+
+    float loss = cross_entropy(id);
+
+    cout << "Cross Entropy Loss: " << loss << endl;
 }
