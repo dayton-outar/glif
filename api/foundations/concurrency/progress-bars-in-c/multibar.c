8543695d28cf1181c8658a41af4db32986a861bd
dayton-outar <dayton.outar@gmail.com>
2021-12-12 15:46:26 -0500

    Using escape sequences

--- api/foundations/concurrency/progress-bars-in-c/multibar.c
+++ api/foundations/concurrency/progress-bars-in-c/multibar.c
@@ -7,9 +7,16 @@
 #include <pthread.h>
 
 const int PROG_BAR_LENGTH = 30;
+const int NUMTHREADS = 5;
 
-void update_bar(int percent_done) {
-    int num_char = percent_done * PROG_BAR_LENGTH / 100;
+typedef struct {
+    int count_to_val;
+    int progress;
+    pthread_t thethread;
+} thread_info;
+
+void update_bar(thread_info* tinfo) {
+    int num_char = (tinfo->progress * 100 / tinfo->count_to_val) * PROG_BAR_LENGTH / 100;
     printf("\r[");
     for (int i = 0; i < num_char; i++) {
         printf("=");
@@ -18,14 +25,44 @@ void update_bar(int percent_done) {
         printf(" ");
     }
 
-    printf("]");
-    
-    fflush(stdout);
+    printf("]\n");
+}
+
+void *generate_thread(void *arg) {
+    thread_info *tinfo = arg;
+
+    for (tinfo->progress = 0; tinfo->progress < tinfo->count_to_val; tinfo->progress++) {
+        usleep(1000);
+    }
+
+    return NULL;
 }
 
 int main() {
-    for (int i = 0; i <= 100; i++) {
-        update_bar(i);
+    thread_info threads[NUMTHREADS];
+
+    for (int i = 0; i < NUMTHREADS; i++) {
+        threads[i].count_to_val = rand() % 10000;
+        threads[i].progress = 0;
+        pthread_create(&threads[i].thethread, NULL, generate_thread, &threads[i]);
+    }
+
+    bool done = false;
+
+    while (!done) {
+        done = true;
+
+        for (int i = 0; i < NUMTHREADS; i++) {
+            update_bar(&threads[i]);
+            if (threads[i].progress < threads[i].count_to_val) {
+                done = false;
+            }
+        }
+        if (!done) {
+            // Escape sequence for ESC and Move up five lines
+            printf("\033[%dF", NUMTHREADS);
+        }
+        
         usleep(10000);
     }
     printf("\nDone!\n");
